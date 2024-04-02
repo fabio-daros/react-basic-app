e6f93bcf0deb304c5c13bdfd42dc68f56eeebd18
Fabio Daros <fabiolaww@gmail.com>
2024-04-02 11:09:47 +0100

    constructing components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Comment from './components/Comment';
 
 function App() {
   return (
     <div className="App">
       <h1>React App</h1>
+      <Comment />
+      <Comment />
+      <Comment />
     </div>
   );
 }
---
+++ src/components/Comment.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Comment.css";
+const Comment = () => (
+    <div className="Comment">
+        <div>John:</div>   
+        <div>Hello, I'm a component!</div>
+    </div>
+);
+
+export default Comment;
