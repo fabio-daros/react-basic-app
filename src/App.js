c6a7825a91be3e394039f51c027bd83ac2278a03
Fabio Daros <fabiolaww@gmail.com>
2024-04-02 12:47:35 +0100

    Manipulating states and inputHandler.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Comment from './components/Comment';
 
 class App extends Component {
 
-  state = { // Stateful component or smart component. It is a class that extends Component and has a state object.
+  state = {               // Stateful component or smart component. It is a class that extends Component and has a state object.
     comments: [
       {
         name: "John",
@@ -19,23 +19,30 @@ class App extends Component {
         date: new Date(2024, 4, 3),
         message: "Hello, I'm fine, thank you!"
       }
-    ]
+    ],
+    newComment: {
+      name: "",
+      email: "",
+      message: ""
+    }
   }
 
-  addComment = () => {
+  addComment = event => {
+    event.preventDefault();
+
     console.log("Adding comment...");
 
-    const newComment = {
-      name: "Mary",
-      email: "mary@mail.com",
-      date: new Date(),
-      message: "Hello, I'm Mary!"
-  }
-  
+    const newComment = { ...this.state.newComment, date: new Date() };
     this.setState({
-      comments: [...this.state.comments, newComment] // Spread operator to add new item in the existing array.
+      comments: [...this.state.comments, newComment],
+      newComment: { name: "", email: "", message: "" } // Clear the form after adding the comment.
     });
-}
+  }
+
+  InputHandler = event => { // Every time the input changes, the state is updated.
+    const { name, value } = event.target;
+    this.setState({ newComment: { ...this.state.newComment, [name]: value } })
+  }
 
   render() {
     return (
@@ -52,8 +59,34 @@ class App extends Component {
           </Comment>
         ))}
 
-
-        <button onClick={this.addComment}>Add comment</button>
+        <form method='post' onSubmit={this.addComment}>
+          <h2>Add a comment</h2>
+          <div>
+            <input
+              type='text'
+              name='name'
+              onChange={this.InputHandler}  // Every time the input changes, the state is updated.
+              value={this.state.newComment.name}
+              placeholder='Name' />
+          </div>
+          <div>
+            <input
+              type='email'
+              name='email'
+              onChange={this.InputHandler}  // Every time the input changes, the state is updated.
+              value={this.state.newComment.email}
+              placeholder='Email' />
+          </div>
+          <div>
+            <textarea
+              name='message'
+              onChange={this.InputHandler}  // Every time the input changes, the state is updated.
+              value={this.state.newComment.message}
+              rows="4">
+            </textarea>
+          </div>
+          <button type='submit'>Comment</button>
+        </form>
       </div>
     );
   }
