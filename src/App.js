afa593f8debcef17443ddb8c1f146e8d4587073f
Fabio Daros <fabiolaww@gmail.com>
2024-04-02 12:01:50 +0100

    working with states

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Comment from './components/Comment';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React App</h1>
-      <Comment name="John" email="john@mail.com" date={new Date(2024, 4, 2)}>
-        Hello, how are you?
-      </Comment>
+class App extends Component {
 
-      <Comment name="Jane" email="jane@mail.com" date={new Date(2024, 4, 3)}>
-        Hello, I'm fine, thank you!
-      </Comment>
-    </div>
-  );
+  state = { // Stateful component or smart component. It is a class that extends Component and has a state object.
+    comments: [
+      {
+        name: "John",
+        email: "john@mail.com",
+        date: new Date(2024, 4, 2),
+        message: "Hello, how are you?"
+      },
+      {
+        name: "Peter",
+        email: "peter@mail.com",
+        date: new Date(2024, 4, 3),
+        message: "Hello, I'm fine, thank you!"
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React App</h1>
+
+        {this.state.comments.map((comment, index) => (
+          <Comment
+            key={index}
+            name={comment.name}
+            email={comment.email}
+            date={comment.date}>
+            {comment.message}
+          </Comment>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./Comment.css";
 
 
-const Comment = props => (
-    <div className="Comment">
+const Comment = props => { // Statless component or dumb component. It is a function that receives props as an argument and returns what should be rendered.
+    return <div className="Comment">
         <h2>{props.name}</h2>
-        <p>{props.email}</p>   
+        <p>{props.email}</p>
         <p>{props.children}</p>
         <p>{props.date.toString()}</p>
     </div>
-);
+};
 
 export default Comment;
